0de485e836c6c33f3a6a4e2a1fbcd091660ac5dd
Nio Kasgami <devil_nicolas@hotmail.com>
2024-01-12 00:08:11 -0500

    pushing the change on window_scrollables which is done

--- libs/rmmz/windows/Window_Scrollable.d.ts
+++ libs/rmmz/windows/Window_Scrollable.d.ts
@@ -25,4 +25,37 @@ declare abstract class Window_Scrollable extends Window_Base {
   scrollX(): number;
   // @ts-ignore
   scrollY(): number;
+  scrollBaseX(): number;
+  scrollBaseY(): number;
+  // @ts-ignore
+  scrollTo(x: number,y: number);
+  // @ts-ignore
+  scrollBy(x: number, y: number);
+  smoothScrollTo(x: number,y: number);
+  smoothScrollBy(x: number,y: number);
+  setScrollAccell(x: number,y: number);
+  overallWidth(): number;
+  overallWeight(): number;
+  maxScrollX(): number;
+  maxScrollY(): number;
+  scrollBlockWidth(): number;
+  scrollBlockHeight(): number;
+  smoothScrollDown(n : number);
+  smoothScrollUp(n: number);
+  override update();
+  processWheelScroll();
+  processTouchScroll();
+  isWheelScrollEnabled(): boolean;
+  isTouchScrollEnabled(): boolean;
+  isScrollEnabled(): boolean;
+  isTouchedInsideFrame(): boolean;
+  onTouchScrollStart();
+  onTouchScroll();
+  onTouchScrollEnd();
+  updateSmoothScroll();
+  updateScrollAccel();
+  updateArrows();
+  updateOrigin();
+  updateScrollBase(baseX: number,baseY: number);
+  abstract paint();
 }
