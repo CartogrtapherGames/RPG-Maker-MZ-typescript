5fe2fe0ba27452f8a051bf4acb9aef96894f1ed2
inc0der <inc0der.dev@gmail.com>
2024-01-11 23:39:36 -0400

    add all managers to rmmz

---
+++ libs/rmmz/managers/EffectManager.d.ts
@@ -0,0 +1,13 @@
+declare namespace EffectManager {
+
+  export function load(filename: string): effekseer.EffekseerEffect;
+  export function startLoading(url: string): effekseer.EffekseerEffect;
+  export function clear(): void;
+  export function onLoad(): void;
+  export function onError(url: string): void;
+  export function makeUrl(filename: string): string;
+  export function checkErrors(): void;
+  export function throwLoadError(url: string): void;
+  export function isReady(): boolean;
+
+}
