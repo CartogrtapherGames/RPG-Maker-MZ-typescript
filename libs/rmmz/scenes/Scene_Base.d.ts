5d5cf5d7f24ecad74c2f853f8b2efabd14f3c017
Nio Kasgami <devil_nicolas@hotmail.com>
2024-02-05 20:01:24 -0500

    Update Scene_Base.d.ts

--- libs/rmmz/scenes/Scene_Base.d.ts
+++ libs/rmmz/scenes/Scene_Base.d.ts
@@ -68,7 +68,7 @@ declare abstract class Scene_Base extends Stage {
 
     public slowFadeSpeed(): number;
 
-    public scaleSprite(): void;
+    public scaleSprite(sprite: Sprite): void;
 
     public centerSprite(sprite: Sprite): void;
 
